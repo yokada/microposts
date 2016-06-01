3f2c183ac8b2b899486132c14009a70cdf3fa256
Yoji Okada <yokada.moo.jp@gmail.com>
2016-06-01 14:12:08 +0900

    add i18n files.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user and @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      flash[:info] = "Logged in as #{@user.name}"
+      flash[:info] = t('flash.logged_in', name: @user.name)
       redirect_to @user
     else
       flash[:danger] = 'Invalid user email/password combination'
       render 'new'
     end
-    
+
   end
 
   def destroy
